d81f28478bcd6be04c4a315d501bb6f3fddc8451
Your Name <you@example.com>
2021-11-23 22:19:53 +0700

    multiply

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ namespace lab_4._2_2
 {
     class Program
     {
-        static void Subtract()
+        static void Multiply()
         {
-            Console.WriteLine("A + B = " + (a - b));
+            Console.WriteLine("A + B = " + (a * b));
         }
     }
 }
