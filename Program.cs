203b5e9c53ba152b179399c4944782d9a6588e64
Your Name <you@example.com>
2021-11-23 20:44:38 +0700

    last commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace lab_4._2
+{
+    class Program
+    {
+
+        delegate void method();
+
+        public static double a;
+        public static double b;
+
+        static void Main(string[] args)
+        {
+            string[] items = { "Ввести А", "Вести В", "выполнить операцию «+» ", "выполнить операцию «-» ", "выполнить операцию «*» ", "выполнить операцию «/» " };
+            method[] methods = new method[] { AddA, AddB, Addition, Subtract, Multiply, Divide, };
+            ConsoleMenu menu = new ConsoleMenu(items);
+            int menuResult;
+            do
+            {
+                menuResult = menu.PrintMenu();
+                methods[menuResult]();
+                Console.WriteLine("Для продолжения нажмите любую клавишу");
+                Console.ReadKey();
+            }
+            while (menuResult != items.Length - 1);
+        }
+    }
+
+
+    class ConsoleMenu
+    {
+        string[] menuItems;
+        int counter = 0;
+        public ConsoleMenu(string[] menuItems)
+        {
+            this.menuItems = menuItems;
+        }
+
+        public int PrintMenu()
+        {
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.Clear();
+                for (int i = 0; i < menuItems.Length; i++)
+                {
+                    if (counter == i)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine(menuItems[i]);
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                        Console.WriteLine(menuItems[i]);
+
+                }
+                key = Console.ReadKey();
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    counter--;
+                    if (counter == -1) counter = menuItems.Length - 1;
+                }
+                if (key.Key == ConsoleKey.DownArrow)
+                {
+                    counter++;
+                    if (counter == menuItems.Length) counter = 0;
+                }
+            }
+            while (key.Key != ConsoleKey.Enter);
+            return counter;
+        }
+
+    }
+}
